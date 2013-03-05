113fa02758ad4a3ecc34882c4b54d0d79e6e0a71
Benjamin Knight <ben@benknight.me>
2013-03-05 11:16:21 -0800

    Commenting JS

--- jquery.shadowedscroller.js
+++ jquery.shadowedscroller.js
@@ -1,14 +1,36 @@
+/**
+ * A fixed-height scrollable region that uses a top and bottom shadow to
+ * indicate scrollability of the content region.
+ *
+ * by Benjamin Knight / MIT License
+ */
 (function($){
 
 	$.fn.shadowedScroller = function(options) {
 
 		return this.each(function() {
 
+			/**
+			 * Plugin options:
+			 *
+			 * 1. height {Number} The fixed height of the scrollable area.
+			 * 2. shadow_size {Number} The size and curvature of the shadow.
+			 * 3. shadow_color {String} The color of the shadow.
+			 */
 			options = $.extend({
-				shadow_size: 30,
-				shadow_color: 'black',
-				height: 200
+				height:       200,
+				shadow_size:  30,
+				shadow_color: 'black'
 			}, options);
+
+			var shadow_css = {
+				'position': 'absolute',
+				'z-index': '1',
+				'width': '100%',
+				'height': options.shadow_size,
+				'border-radius': '50%',
+				'box-shadow': '0 0 ' + options.shadow_size + 'px ' + options.shadow_color
+			};
 
 			var $this = $(this),
 				container = $([
@@ -25,6 +47,7 @@
 				inner   = container.find('.ss-inner'),
 				bottom  = container.find('.ss-bottom');
 
+			// Transition function that fades in and out quickly, inverse ease-in-out.
 			var hardInOut = function(val) {
 				return 4 * Math.pow((val - 0.5), 3) + 0.5;
 			};
@@ -36,7 +59,7 @@
 				bottom.css({ 'opacity': hardInOut(1 - ( s / h )) });
 			};
 
-			// styles
+			// set styles
 			container.css({
 				'position': 'relative',
 				'overflow': 'hidden'
@@ -47,22 +70,14 @@
 				'height': options.height
 			});
 
-			var shadow_css = {
-				'position': 'absolute',
-				'z-index': '1',
-				'width': '100%',
-				'height': options.shadow_size,
-				'border-radius': '50%',
-				'box-shadow': '0 0 ' + options.shadow_size + 'px ' + options.shadow_color
-			};
-
 			top.css(shadow_css).css('top', -1 * options.shadow_size);
 			bottom.css(shadow_css).css('bottom', -1 * options.shadow_size);
 
-			content.bind('scroll', setOpacity );
+			content.bind('scroll', setOpacity);
 			container.insertBefore($this);
 			$this.appendTo(inner);
 			setOpacity();
 		});
 	};
+
 })(jQuery);
